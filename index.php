ac1f84378d49e6b9499d2c923af645619f45b139
kw1c-KaddouriAdam <adam.kaddouri@ecu-kw1c.nl>
2025-03-11 11:48:28 +0100

    eerste push

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-    phpinfo()
+/**
+ * User: Adam kaddouri, Jesper sterrenburg en Robert bogmans
+ * Date: 11-3-2025
+ * File: index.php
+ */
 ?>
+<!DOCTYPE html>
+<html lang="nl" xmlns="http://www.w3.org/1999/html">
+<head>
+    <title> </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/style.css" rel="stylesheet">
+</head>
+<body>
+<main>
+</main>
+</body>
+</html>
+
+
