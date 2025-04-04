3adbee9b95da537acef828cd5c7d60e6e095bcfc
kw1c-SterrenburgJesper <jesper.sterrenburg@edu-kw1c.nl>
2025-04-04 11:53:07 +0200

    opmaak

--- Includes/Navigator.php
+++ Includes/Navigator.php
@@ -6,13 +6,15 @@
  * File: Navigator.php
  */
 ?>
-
+<!--img ingevoegd met figure en img-->
 <figure>
     <img id="logo" src="../images/klm.png">
 </figure>
-
+<!--nav ingevoegd -->
     <nav>
+<!--        ul ingevoegd -->
         <ul>
+<!--            meerdere keren li ingevoegd met een class en een a ingevoegd waar een link instaat naar een andere pagina en met blank word de pagina in een andere tablad geopend -->
             <li class="linav"><a class="anav" target="_blank" href="../index.php">Home</a></li>
             <li class="linav"><a class="anav" target="_blank" href="https://www.klm.nl/trip">Mijn reis</a></li>
             <li class="linav"><a class="anav" target="_blank" href="https://www.klm.nl/check-in">Check-in</a></li>
@@ -20,6 +22,7 @@
             <li class="linav"><a class="anav" target="_blank" href="https://www.klm.nl/information/support">Hulp</a></li>
         </ul>
     <nav>
+<!--        streep ingevoegd-->
         <hr id="streepjenav">
 
 
